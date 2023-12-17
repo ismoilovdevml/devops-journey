74ff80c6c28130b182d979d531151fd0c3dbef66
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-12-17 14:17:07 +0500

    [FIX] bugs fixed

--- components/icons/github-sponsors.tsx
+++ components/icons/github-sponsors.tsx
@@ -1,23 +1,19 @@
 export function GithubSponsors() {
-    return (
-      <>
-        <a
-        >
-        </a>
-        <a
-          href="https://github.com/sponsors/ismoilovdevml"
-          target="_blank"
-          className="hidden sm:block"
-        >
-          <iframe
-            src="https://github.com/sponsors/ismoilovdevml/button"
-            title="Sponsor ismoilovdev"
-            height="32"
-            width="114"
-            className="rounded-md border-0 dark:invert invert-0"
-          ></iframe>
-        </a>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <a
+        href="https://github.com/sponsors/ismoilovdevml"
+        target="_blank"
+        className="hidden sm:block"
+      >
+        <iframe
+          src="https://github.com/sponsors/ismoilovdevml/button"
+          title="Sponsor ismoilovdev"
+          height="32"
+          width="114"
+          className="rounded-md border-0 dark:invert invert-0"
+        ></iframe>
+      </a>
+    </>
+  );
+}
