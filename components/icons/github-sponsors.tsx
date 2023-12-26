30b436b0cc9a264e64c1b02c574af1a6930ccdf1
dastanbayev.s <samiddastanbayev@gmail.com>
2023-12-26 12:24:50 +0500

    danger

--- components/icons/github-sponsors.tsx
+++ components/icons/github-sponsors.tsx
@@ -10,10 +10,6 @@ export function GithubSponsors() {
           src="https://github.com/sponsors/ismoilovdevml/button"
           title=""
           height="32"
-          width="114"
-          className="rounded-md border-0 dark:invert invert-0"
-        ></iframe>
-      </a>
     </>
   );
 }
